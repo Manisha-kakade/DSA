3bcd9b566e472900d712a9c7b7cc6aab48cea749
Manisha-kakade <manishakakade95@gmail.com>
2025-06-15 16:17:11 +0530

    programming 101 basics

--- programming101.js
+++ programming101.js
@@ -1,1 +1,50 @@
 console.log('programming 101')
+// Strings 
+let str = "hello world!";
+console.log(str)
+
+//accessing the string letter
+console.log(str[1])
+
+//concating thestring
+ 
+console.log(str + ' ' + "There!")
+
+// number
+
+let num = 10;
+console.log(num);
+ 
+// adding numbers
+console.log(num + 20)
+
+//Boolean
+let flag = true //false
+console.log(flag)
+
+//Array
+let arr = [1,2,3,4,5]
+
+//accessing array
+console.log(arr[2])
+
+//adding array element
+console.log(arr[2] + arr[4])
+
+//Nested Array
+
+let arr1 = [1,2,[3,4,[5,6]]]
+console.log('arr1[2][2][0] : ',arr1[2][2][0])
+
+//Objects
+
+let obj = {
+    name:'Dylan',
+    age:20
+}
+
+//accessing obj
+console.log('obj.name: ',obj.name)
+console.log("obj['name']: ",obj['name'])
+console.log("obj?.['name']: ",obj?.['name'])
+
